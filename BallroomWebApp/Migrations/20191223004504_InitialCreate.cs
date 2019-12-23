a552c29dd359ef95c9859cd137cbbb6cd04b1e0c
Michael Navazhylau <mikipux7@gmail.com>
2019-12-22 18:54:22 -0600

    Rest of stuff

---
+++ BallroomWebApp/Migrations/20191223004504_InitialCreate.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BallroomWebApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dance",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Speed = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dance", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DanceVideo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    VideoUrl = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DanceVideo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Syllabus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Level = table.Column<int>(nullable: false),
+                    DanceId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Syllabus", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Syllabus_Dance_DanceId",
+                        column: x => x.DanceId,
+                        principalTable: "Dance",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DanceMove",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    VideoId = table.Column<int>(nullable: true),
+                    SyllabusId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DanceMove", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DanceMove_Syllabus_SyllabusId",
+                        column: x => x.SyllabusId,
+                        principalTable: "Syllabus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_DanceMove_DanceVideo_VideoId",
+                        column: x => x.VideoId,
+                        principalTable: "DanceVideo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DanceMove_SyllabusId",
+                table: "DanceMove",
+                column: "SyllabusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DanceMove_VideoId",
+                table: "DanceMove",
+                column: "VideoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Syllabus_DanceId",
+                table: "Syllabus",
+                column: "DanceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DanceMove");
+
+            migrationBuilder.DropTable(
+                name: "Syllabus");
+
+            migrationBuilder.DropTable(
+                name: "DanceVideo");
+
+            migrationBuilder.DropTable(
+                name: "Dance");
+        }
+    }
+}
